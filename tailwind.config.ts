565dcab7f2eeaf919f029e3bbe286a93c9358451
DisRachik <radchenkoig@gmail.com>
2024-04-24 19:39:46 +0300

    init: project settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			white: '#ffffff',
+			light: '#D9D9D9',
+			error: '#FF5757',
+		},
+		screens: {
+			xs: '320px',
+			...defaultTheme.screens,
+			sm: '480px',
+		},
+		extend: {
+			backgroundImage: {},
+		},
+	},
+	plugins: [],
 };
 export default config;

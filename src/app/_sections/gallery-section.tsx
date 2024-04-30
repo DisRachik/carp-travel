c679754b771f1c3f18babc7a1a36c43b4d5c364d
DisRachik <radchenkoig@gmail.com>
2024-04-30 16:20:34 +0300

    feat: add slider to gallery

--- src/app/_sections/gallery-section.tsx
+++ src/app/_sections/gallery-section.tsx
@@ -1,16 +1,41 @@
+'use client';
+
+import clsx from 'clsx';
+import Image from 'next/image';
 import { Section, Container, Title } from '@/components';
 import { section } from '@/components/section';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
 import content from '@/data/content.json';
 const { title, cards } = content.gallery;
+const images = [...cards, ...cards];
 
 const GallerySection = () => {
 	return (
 		<Section sectionTitle={section.Gallery} className=''>
 			<Container className=''>
-				<Title sectionName={section.Gallery} elementName='h2' className=''>
+				<Title sectionName={section.Gallery} elementName='h2' className='mb-6'>
 					{title}
 				</Title>
+
+				<Swiper
+					className={clsx('h-[633px]', 'mySwiper')}
+					direction='vertical'
+					slidesPerView={3}
+					spaceBetween={24}
+					loop={true}
+				>
+					{images.map(({ image, alt, id }, itemId) => {
+						const key = +id > 3 ? id : itemId;
+						return (
+							<SwiperSlide key={key} className='relative'>
+								<Image src={image} alt={alt} fill />
+							</SwiperSlide>
+						);
+					})}
+				</Swiper>
 			</Container>
 		</Section>
 	);
